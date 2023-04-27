4c39f09a13bcdf1433ba072205e3f82e59b0ea79
unknown <nandagb23@gmail.com>
2023-04-27 13:33:04 -0300

    removendo projeto antigo

--- PlanIt/src/Projeto/Menu.java
+++
@@ -1,7 +0,0 @@
-package Projeto;
-
-public interface Menu {
-	public void exibirConteudo();
-	public void receberEntrada(int opcao, Projeto projeto);
-	public void receberEntrada(String opcao, Projeto projeto);
-}
