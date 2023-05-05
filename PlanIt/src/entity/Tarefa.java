ac0f1d4dcf510c79f5a257fa61e576d4618d730a
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-05-05 14:58:35 -0300

    adicionada entidade tarefa

---
+++ PlanIt/src/entity/Tarefa.java
@@ -0,0 +1,56 @@
+package entity;
+
+import java.sql.Date;
+
+public class Tarefa {
+    public int id;
+    public String nome;
+    public String descricao;
+    public boolean status;
+    public Date prazo;
+    public int project_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(Date prazo) {
+        this.prazo = prazo;
+    }
+
+    public int getProject_id() {
+        return project_id;
+    }
+
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
+    }
+}
