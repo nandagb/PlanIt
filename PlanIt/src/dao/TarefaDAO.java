493ae4a18c18735c4314fdef152ea4d13788cc7d
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-05-06 11:56:18 -0300

    adicionados comentarios na interface de TarefaDao

--- PlanIt/src/dao/TarefaDAO.java
+++ PlanIt/src/dao/TarefaDAO.java
@@ -6,18 +6,21 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface TarefaDAO {
-
-    void save(Tarefa tarefa) throws SQLException;
-
-    boolean existe(Tarefa tarefa);
-
     Tarefa findById(int id);
 
     Tarefa findByName(String name, int project_id);
+//  SALVA UMA TAREFA NO BANCO DE DADOS.
+    void save(Tarefa tarefa) throws SQLException;
 
+//  VERIFICA SE UMA TAREFA DE MESMO ID JÁ EXISTE NO BANCO DE DADOS.
+    boolean existe(Tarefa tarefa);
+
+//  RETORNA UMA LISTA COM TODAS AS TAREFAS EM UM PROJETO.
     List<Tarefa> findAllOnProject(int project_id);
 
+//  ATUALIZA A ENTRADA DE UMA TAREFA COM OS SEUS VALORES ATUAIS.
     void update(Tarefa tarefa);
 
+//  DELETA A ENTRADA DE UMA TAREFA COM O ID PASSADO NO BANCO DE DADOS.
     void delete(int id);
 }
