847d294f362b6391be0968b8d15dfd77da78bf00
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-05-06 20:40:33 -0300

    criada validação de tarefas

--- PlanIt/src/controller/TarefaController.java
+++ PlanIt/src/controller/TarefaController.java
@@ -2,9 +2,10 @@ package controller;
 
 import dao.TarefaDAO;
 import dao.TarefaDAOImpl;
+import entity.Tarefa;
 
 public class TarefaController {
-    public static boolean criarTarefa(T){
+    public static boolean criarTarefa(Tarefa tarefa){
         TarefaDAO dao = new TarefaDAOImpl();
 
     }
--- PlanIt/src/services/TarefaServices.java
+++ PlanIt/src/services/TarefaServices.java
@@ -1,14 +1,29 @@
 package services;
 
+import dao.TarefaDAO;
+import dao.TarefaDAOImpl;
 import entity.Tarefa;
 
 import java.sql.Date;
+import java.sql.SQLException;
 
 public class TarefaServices {
-    public static void validaTarefa(Tarefa tarefa){}
+    public static boolean validaTarefa(Tarefa tarefa){
+        if(prazoValido(tarefa) && nomeValido(tarefa) && statusValido(tarefa)){
+            try {
+                TarefaDAO dao = new TarefaDAOImpl();
+                dao.save(tarefa);
+                return true;
+            }
+            catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+        return false;
+    }
 
 //  CHECA SE A DATA DO PRAZO É POSTERIOR A DATA ATUAL
-    public boolean prazoValido(Tarefa tarefa){
+    public static boolean prazoValido(Tarefa tarefa){
         long timeStampAtual = System.currentTimeMillis();
         Date dataAtual = new Date(timeStampAtual);
         if(dataAtual.compareTo(tarefa.getPrazo()) <= 0){
@@ -18,7 +33,7 @@ public class TarefaServices {
     }
 
 //    VERIFICA SE O NOME É VÁLIDO
-    public boolean nomeValido(Tarefa tarefa){
+    public static boolean nomeValido(Tarefa tarefa){
         if(tarefa.getNome().length() > 0){
             return true;
         }
@@ -26,7 +41,7 @@ public class TarefaServices {
     }
 
 //    CHECA SE O STATUS PASSADO É VÁLIDO
-    public boolean statusValido(Tarefa tarefa){
+    public static boolean statusValido(Tarefa tarefa){
         if(tarefa.getStatus() == "em_andamento" || tarefa.getStatus() == "finalizado" || tarefa.getStatus() == "atrasado"){
             return true;
         }
