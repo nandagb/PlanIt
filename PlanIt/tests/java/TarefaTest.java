61d13531e763bee01a94284ae61f5aebe550b026
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-05-07 20:11:50 -0300

    atualiados testes

--- PlanIt/tests/java/TarefaTest.java
+++ PlanIt/tests/java/TarefaTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import entity.Tarefa;
 import controller.TarefaController;
 import org.junit.jupiter.api.BeforeAll;
@@ -32,8 +35,32 @@ public class TarefaTest {
     }
 
     @Test
+    public void UpdateTarefa(){
+        tarefa.setNome("1234");
+        boolean update = TarefaController.atualizarTarefa(tarefa);
+        assertEquals("1234", tarefa.getNome());
+    }
+    @Test
+    public void BuscaTarefaIdTest(){
+        Tarefa achado = TarefaController.acharTarefaId(tarefa.getId());
+        assertEquals(achado.getId(), tarefa.getId());
+    }
+    @Test
+    public void BuscaTarefaNomeTeste(){
+        Tarefa achado = TarefaController.acharTarefaNome(tarefa.getNome(), 1);
+        assertEquals(tarefa.getId(), achado.getId());
+    }
+    @Test
+    public void BuscaTodasTarefasTeste(){
+        List<Tarefa> achados = TarefaController.acharTarefasProjeto(1);
+        System.out.println("Tarefas do projeto " + TarefaController.acharTarefaId(tarefa.getProject_id()).getNome()
+                + "\n");
+        achados.forEach(tarefa -> System.out.println(tarefa.getNome()));
+    }
+
+    @Test
     public void DeletaTarefaTest() throws SQLException {
-        boolean delecao = TarefaController.deletarTarefa(this.tarefa);
+        boolean delecao = TarefaController.deletarTarefa(tarefa);
         assertTrue(delecao);
     }
 }
